74e92631326651160a804fb04122ed2675cf2833
loekTheDreamer <luke.carelsen@me.com>
2022-02-17 15:52:31 +0200

    initial errors

---
+++ App.tsx
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import HelloWorldSceneAR from './src/example/HelloWorldSceneAR';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <HelloWorldSceneAR />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ metro.config.js
@@ -0,0 +1,54 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+// module.exports = {
+//   transformer: {
+//     getTransformOptions: async () => ({
+//       transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: true,
+//       },
+//     }),
+//   },
+
+// };
+
+// Learn more https://docs.expo.io/guides/customizing-metro
+const {getDefaultConfig} = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: {assetExts},
+  } = await getDefaultConfig();
+
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: [
+        ...assetExts,
+        'obj',
+        'mtl',
+        'mp3',
+        'JPG',
+        'vrx',
+        'hdr',
+        'gltf',
+        'glb',
+        'bin',
+        'arobject',
+        'gif',
+      ],
+    },
+  };
+})();
